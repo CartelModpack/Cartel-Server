d210e36dd841a5369a733e7b0da3cfd3cb0fd912
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-03-22 00:40:37 -0500

    Init Server

---
+++ config.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+
+const DEFAULT_CONF = {
+    port: 8080,
+    protected_dir: path.join(__dirname, "./protected")
+}
+
+let final = DEFAULT_CONF;
+if (!fs.existsSync(path.join(__dirname, "./conf/config.json"))) {
+    if (!fs.existsSync(path.join(__dirname, "./conf"))) fs.mkdirSync(path.join(__dirname, "./conf"));
+    fs.writeFileSync(path.join(__dirname, "./conf/config.json"), JSON.stringify(DEFAULT_CONF, null, 2), "utf-8");
+} else {
+    final = JSON.parse(fs.readFileSync(path.join(__dirname, "./conf/config.json"), "utf-8"));
+}
+
+module.exports = final;
---
+++ index.js
@@ -0,0 +1,18 @@
+// Modules
+const http = require("http");
+const express = require("express");
+
+// Config
+const conf = require("./config")
+
+// Server
+const app = express();
+const server = http.createServer(app);
+
+// Express Routes
+app.use(express.static(conf.private_dir));
+
+// Launch
+server.listen(conf.port, _ => {
+    console.info(`Cartel Server Live at *:${conf.port}`);
+})
