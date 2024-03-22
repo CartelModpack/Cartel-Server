647a542604258af62532c3a81542a3b7a19efd13
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-03-22 01:34:03 -0500

    Added cartel list

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Modules
 const http = require("http");
 const express = require("express");
+const path = require("path");
 
 // Config
 const conf = require("./config")
@@ -10,6 +11,9 @@ const app = express();
 const server = http.createServer(app);
 
 // Express Routes
+app.use("/latest/lite", express.static(path.join(__dirname, "./Cartel/lite")));
+app.use("/latest/normal", express.static(path.join(__dirname, "./Cartel/normal")));
+app.use("/latest/plus", express.static(path.join(__dirname, "./Cartel/plus")));
 app.use("/protected", express.static(conf.private_dir));
 
 // Launch
